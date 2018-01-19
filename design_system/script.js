50e7b90be1876c5243a95a6e3d15f99bf5ff482e
Xian Chen <xchen@Xians-MacBook-Pro.local>
2018-01-19 14:57:22 -0800

    adding design sys files

---
+++ design_system/script.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+
+  function removeWeightClass(element){
+      var weights= ['bds-txt-light', 'bds-txt-regular', 'bds-txt-semi-bold', 'bds-txt-bold'];
+      for (var i = 0; i < weights.length; i++) {
+          element.removeClass(weights[i]);
+      }
+    }
+
+
+  $('.typography-weights > span').hover(function(){
+    var changedWeight=$(this).attr('class');
+    $('.heading-prefix').html(changedWeight);
+
+    /* change weight of headings */
+    $('.typography-headings *').attr('class', changedWeight);
+
+    /* change weight for subheader, body, and legal*/
+    $('.typography-other > div').each(function(index) {
+      if($(this).hasClass('bds-txt-light') || $(this).hasClass('bds-txt-regular') || $(this).hasClass('bds-txt-bold') || $(this).hasClass('bds-txt-semi-bold')){
+        removeWeightClass($(this));
+        $(this).addClass(changedWeight);
+      }
+      else
+        $(this).addClass(changedWeight);
+    });
+
+  });
+
+  $('.showClassNames').click(function(){
+    var parentClass = '.'+$(this).parent().attr('class');
+    console.log(parentClass+' .classNames');
+    $(parentClass+' .plusMinus').toggleClass('active');
+    $(parentClass+' .classNames').toggle();
+  });
+  
+});
