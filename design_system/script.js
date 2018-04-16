602aabe64d5763e576f0f3cda00a91c0b007dfcf
Xian Chen <xc.dsgn@gmail.com>
2018-04-16 14:11:11 -0700

    updated documentation

--- design_system/script.js
+++ design_system/script.js
@@ -1,36 +1,45 @@
 $(document).ready(function() {
-  // typography font weight hovering functionality
-  function removeWeightClass(element){
-    var weights= ['bds-txt-light', 'bds-txt-regular', 'bds-txt-semi-bold', 'bds-txt-bold'];
-    for (var i = 0; i < weights.length; i++) {
-        element.removeClass(weights[i]);
-    }
-  }
-
-  $('.typography-weights > span').hover(function(){
-    var changedWeight=$(this).attr('class');
-    $('.heading-prefix').html(changedWeight);
-    /* change weight of headings */
-    $('.typography-headings *').attr('class', changedWeight);
-    /* change weight for subheader, body, and legal*/
-    $('.typography-other > div').each(function(index) {
-      if($(this).hasClass('bds-txt-light') || $(this).hasClass('bds-txt-regular') || $(this).hasClass('bds-txt-bold') || $(this).hasClass('bds-txt-semi-bold')){
-        removeWeightClass($(this));
-        $(this).addClass(changedWeight);
-      }
-      else
-        $(this).addClass(changedWeight);
-    });
+  //hamburger
+  $('.hamburger').on('click', function(){
+    $('header nav').toggleClass('active');
   });
+
+  $('nav ul li a').on('click', function(){
+    $('header nav').removeClass('active');
+  });
+
+
+  // typography font weight hovering functionality
+  // function removeWeightClass(element){
+  //   var weights= ['bds-txt-light', 'bds-txt-regular', 'bds-txt-semi-bold', 'bds-txt-bold'];
+  //   for (var i = 0; i < weights.length; i++) {
+  //       element.removeClass(weights[i]);
+  //   }
+  // }
+
+  // $('.typography-weights > span').hover(function(){
+  //   var changedWeight=$(this).attr('class');
+  //   $('.heading-prefix').html(changedWeight);
+  //   /* change weight of headings */
+  //   $('.swap-weight *').attr('class', changedWeight);
+  //   /* change weight for subheader, body, and legal*/
+  //   $('.typography-other > div').each(function(index) {
+  //     if($(this).hasClass('bds-txt-light') || $(this).hasClass('bds-txt-regular') || $(this).hasClass('bds-txt-bold') || $(this).hasClass('bds-txt-semi-bold')){
+  //       removeWeightClass($(this));
+  //       $(this).addClass(changedWeight);
+  //     }
+  //     else
+  //       $(this).addClass(changedWeight);
+  //   });
+  // });
+
 
   //toggling examples
-  $('.exampleToggle').click(function(){
-    $('.plusMinus').toggleClass('active');
-    $('.showHide').toggleClass('active');
-    $('.example').toggle();
-  });
-
-
+  // $('.exampleToggle').click(function(){
+  //   $('.plusMinus').toggleClass('active');
+  //   $('.showHide').toggleClass('active');
+  //   $('.example').toggle();
+  // });
 
 
 
@@ -69,7 +78,7 @@ $(document).ready(function() {
     var in_view_typography = Utils.isElementInView($('#typography'), false);
     var in_view_colorPalette = Utils.isElementInView($('#colorPalette'), false);
     var in_view_spacing = Utils.isElementInView($('#spacing'), false);
-    var in_view_breakpoints = Utils.isElementInView($('#breakpoints'), false);
+    // var in_view_breakpoints = Utils.isElementInView($('#breakpoints'), false);
     var in_view_buttons = Utils.isElementInView($('#buttons'), false);
     var in_view_utilities = Utils.isElementInView($('#utilities'), false);
     var in_view_hideshow= Utils.isElementInView($('#hideshow'), false);
@@ -79,7 +88,7 @@ $(document).ready(function() {
     if (in_view_typography==true){ boldNavItem('typography')}
     if (in_view_colorPalette==true){ boldNavItem('colorPalette')}
     if (in_view_spacing==true){ boldNavItem('spacing')}
-    if (in_view_breakpoints==true){ boldNavItem('breakpoints')}
+    // if (in_view_breakpoints==true){ boldNavItem('breakpoints')}
     if (in_view_buttons==true){ boldNavItem('buttons')}
     if (in_view_utilities==true){ boldNavItem('utilities')}
     if (in_view_hideshow==true){ boldNavItem('hideshow')}
